b4ee8cf9fc4083468dbb2ff6a1a11113e24547a9
Nikita-Komintsev <nikita.comin@gmail.com>
2023-08-13 19:57:41 +0300

    pre-final

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 const link = "http://api.weatherstack.com/current?access_key=f982ebfb9deddf0e927ec524c39ae958";
 
 const root = document.getElementById('root');
+const popup = document.getElementById('popup');
+const textInput = document.getElementById('text-input');
+const form = document.getElementById('form');
+const close = document.getElementById('close');
 
 let store = {
     city: "Moscow",
-    feelslike: 0,
-    cloudcover: 0,
     temperature: 0,
-    humidity: 0,
     observationTime: "00:00 AM",
-    pressure: 0,
-    uvIndex: 0,
-    visibility: 0,
     isDay: "yes",
     description: "",
-    windSpeed: 0,
+    properties: {
+        cloudcover: {},  
+        humidity: {},  
+        windSpeed: {},
+        visibility: {}, 
+        pressure: {},
+        uvIndex: {}, 
+    }
 }
 
 const fetchData = async () => {
-    const result = await fetch(`${link}&query=${store.city}`);
+    try {
+        const result = await fetch(`${link}&query=${store.city}`);
     const data = await result.json();
 
     const { 
         current: {
-            feelslike, 
             cloudcover, 
             temperature, 
             humidity,
@@ -33,30 +38,153 @@ const fetchData = async () => {
             visibility,
             is_day:isDay,
             weather_descriptions:description,
-            wind_speed:windSpeed 
+            wind_speed:windSpeed,
         }, 
     } = data;
     
     store = {
         ...store,
-        feelslike,
-        cloudcover,
         temperature,
-        humidity,
         observationTime,
-        pressure,
-        uvIndex,
-        visibility,
         isDay,
         description:description[0],
-        windSpeed
+        properties: {
+            cloudcover: {
+                title: "cloudcover",
+                value: `${cloudcover}`,
+                icon: "cloud.png"
+            },  
+            humidity: {
+                title: "humidity",
+                value: `${humidity}%`,
+                icon: "humidity.png"
+            },
+            windSpeed: {
+                title: "windSpeed",
+                value: `${windSpeed} km/h`,
+                icon: "wind.png"
+            },
+            visibility: {
+                title: "visibility",
+                value: `${visibility}%`, 
+                icon: "visibility.png"
+            },
+            pressure: {
+                title: "pressure",
+                value: `${pressure}%`,
+                icon: "gauge.png"
+            },
+            uvIndex: {
+                title: "uvIndex",
+                value: `${uvIndex} / 100`,
+                icon: "uv-index.png"
+            } 
+        }
     }
 
     renderComponent();
+    } catch(err){
+        console.log(err);
+    }
+};
+
+const getImage = (description) => { 
+    const value = description.toLowerCase();
+
+    switch(value){
+        case "partly cloudy":
+            return "partly.png";
+        case "cloud":
+            return "cloud.png";
+        case "clear":
+            return "clear.png";
+        case "fog":
+            return "fog.png";
+        case "sunny":
+            return "sunny.png";
+        default:
+            return "the.png";
+    }
+};
+
+const renderProperty = (properties) => {
+    return Object.values(properties).map(({title, value, icon}) =>{
+
+        return `<div class="property">
+                    <div class="property-icon">
+                        <img src="./img/icons/${icon}" alt="">
+                    </div>
+                    <div class="property-info">
+                        <div class="property-info__value">${value}</div>
+                        <div class="property-info__description">${title}</div>
+                    </div>
+                </div>`;
+    }).join("");
+};
+
+const markup = () => {
+    const { city, description, observationTime, temperature, isDay, properties} = store;
+    const containerClass = isDay === "yes" ? "is-day" : "";
+
+    return `<div class="container ${containerClass}">
+                <div class="top">
+                    <div class="city">
+                        <div class="city-subtitle">Weather Today in</div>
+                        <div class="city-title" id="city">
+                        <span>${city}</span>
+                    </div>
+                </div>
+                <div class="city-info">
+                    <div class="top-left">
+                    <img class="icon" src="./img/${getImage(description)}" alt="" />
+                    <div class="description">${description}</div>
+                </div>
+                
+                <div class="top-right">
+                    <div class="city-info__subtitle">${observationTime}</div>
+                    <div class="city-info__title">${temperature}°</div>
+                </div>
+            </div>
+            </div>
+                <div id="properties">${renderProperty(properties)}</div>
+            </div>`;
+};
+
+const togglePopupClass = () => {
+    popup.classList.toggle('active');
 };
 
 const renderComponent = () => {
-    root.innerHTML = `${store.temperature}°`;
+    root.innerHTML = markup();
+
+    const city = document.getElementById('city');
+    city.addEventListener('click', togglePopupClass);
 };
+
+const handleInput = (e) => {
+    store = {
+        ...store,
+        city: e.target.value,
+    };
+};
+
+const handleSubmit = (e) => {
+   e.preventDefault();
+
+   if(!store.city) return null;
+
+//    localStorage.setItem('query')
+
+   fetchData();
+   togglePopupClass();
+};
+
+const handleClose = () => {
+    popup.classList.toggle('popup-close');
+};
+
+form.addEventListener('submit', handleSubmit)
+textInput.addEventListener('input', handleInput);
+close.addEventListener('click', handleClose)
 
 fetchData();
